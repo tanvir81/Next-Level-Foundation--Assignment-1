cbeb355023af30fc3a0d381be3a3ca3dd5910b4c
tanvir81 <tanvirkha2019@gmail.com>
2026-09-02 15:17:26 +0600

    Question-1 Answer

--- answer.js
+++ answer.js
@@ -1,1 +1,7 @@
 //Next Level Foundation Program-Assignment-1
+// Question 1 Answer
+function describeValue(value) {
+  const typeString = typeof value;
+  const truthyValue = value ? "truthy" : "falsy";
+  return `${typeString} | ${truthyValue}`;
+}
